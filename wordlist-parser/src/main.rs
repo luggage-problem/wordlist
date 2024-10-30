33a5d65681c27a4a4dd38406adc506ae4569a898
Duncan <?>
2024-10-30 14:21:22 -0700

    Updates.

--- wordlist-parser/src/main.rs
+++ wordlist-parser/src/main.rs
@@ -13,9 +13,9 @@ enum State {
     VariableSeparator,
     Error,
     VariableList,
-    EOF,
+    Eof,
     InlineVariable,
-    InlineVariableSeparator
+    InlineVariableSeparator,
 }
 
 struct Transition {
@@ -50,11 +50,12 @@ fn start_variable_key(parser: &mut Parser) {
 fn add_char_to_variable_key(parser: &mut Parser) {
     *parser.current_var_key.as_mut().unwrap() += &parser.current_char.to_string();
 }
-fn end_variable_key(parser: &mut Parser) {
+fn end_variable_key(_: &mut Parser) {
     // println!("{:?}", parser.current_var_key.as_ref().unwrap());
 }
 fn start_inline_variable(parser: &mut Parser) {
-    parser.current_var_key = Some("__INLINE_VARIABLE".to_owned());
+    parser.current_var_key = Some("score".to_owned());
+    parser.current_var_value = Some(parser.current_char.to_string());
 }
 fn start_variable_value(parser: &mut Parser) {
     parser.current_var_value = Some(parser.current_char.to_string());
@@ -81,7 +82,7 @@ struct Parser {
     current_word: Option<Word>,
     current_var_key: Option<String>,
     current_var_value: Option<String>,
-    parsed_words: Vec<Word>
+    parsed_words: Vec<Word>,
 }
 
 fn main() {
@@ -91,7 +92,7 @@ fn main() {
         current_word: None,
         current_var_key: None,
         current_var_value: None,
-        parsed_words: Vec::new()
+        parsed_words: Vec::new(),
     };
     let transitions = vec![
         // whitespace
@@ -99,222 +100,220 @@ fn main() {
             source: State::Whitespace,
             destination: State::Whitespace,
             condition: Regex::new(r"[\t ]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Whitespace,
             destination: State::Comment,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Whitespace,
             destination: State::Newline,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Whitespace,
             destination: State::Error,
             condition: Regex::new(r":").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Whitespace,
             destination: State::VariableKey,
             condition: Regex::new(r"[a-zA-Z]").unwrap(),
-            callback: Box::new(|mut p| start_variable_key(&mut p)),
+            callback: Box::new(start_variable_key),
         },
         // newline
         Transition {
             source: State::Newline,
             destination: State::Whitespace,
             condition: Regex::new(r"[ \t]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Newline,
             destination: State::Comment,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Newline,
             destination: State::Newline,
             condition: Regex::new(r"\t").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Newline,
             destination: State::Error,
             condition: Regex::new(r":").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Newline,
             destination: State::Word,
             condition: Regex::new(r"[a-zA-Z]").unwrap(),
-            callback: Box::new(|mut p| start_word(&mut p)),
+            callback: Box::new(start_word),
         },
         // Comment
         Transition {
             source: State::Comment,
             destination: State::Comment,
             condition: Regex::new(r"[^\n]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Comment,
             destination: State::Newline,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         // Word
         Transition {
             source: State::Word,
             destination: State::Word,
             condition: Regex::new(r"[^ ;:#\n]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_word(&mut p)),
+            callback: Box::new(add_char_to_word),
         },
         Transition {
             source: State::Word,
             destination: State::Whitespace,
             condition: Regex::new(r"[ \t]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Word,
             destination: State::Comment,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Word,
             destination: State::Newline,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::Word,
             destination: State::InlineVariableSeparator,
             condition: Regex::new(r"[;:]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         // InlineVariableSeparator
         Transition {
             source: State::InlineVariableSeparator,
             destination: State::InlineVariable,
-            condition: Regex::new(r"[;:]").unwrap(),
-            callback: Box::new(|mut p| start_inline_variable(&mut p)),
+            condition: Regex::new(r"[^ \t#\n]").unwrap(),
+            callback: Box::new(start_inline_variable),
         },
-
         // InlineVariable
-
         Transition {
             source: State::InlineVariable,
             destination: State::InlineVariable,
-            condition: Regex::new(r"[^ #\n]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_word(&mut p)),
+            condition: Regex::new(r"[^ \t#\n]").unwrap(),
+            callback: Box::new(add_char_to_variable_value),
         },
         Transition {
             source: State::InlineVariable,
             destination: State::Whitespace,
             condition: Regex::new(r"[ \t]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(end_variable_value),
         },
         Transition {
             source: State::InlineVariable,
             destination: State::Comment,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(end_variable_value),
         },
         Transition {
             source: State::InlineVariable,
             destination: State::Newline,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(end_variable_value),
         },
-
         // VariableKey
         Transition {
             source: State::VariableKey,
             destination: State::VariableKey,
             condition: Regex::new(r"[^ #\n:]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_variable_key(&mut p)),
+            callback: Box::new(add_char_to_variable_key),
         },
         Transition {
             source: State::VariableKey,
             destination: State::VariableKey,
             condition: Regex::new(r"[ \t]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_variable_key(&mut p)),
+            callback: Box::new(add_char_to_variable_key),
         },
         Transition {
             source: State::VariableKey,
             destination: State::Error,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::VariableKey,
             destination: State::VariableList,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         Transition {
             source: State::VariableKey,
             destination: State::VariableSeparator,
             condition: Regex::new(r":").unwrap(),
-            callback: Box::new(|mut p| end_variable_key(&mut p)),
+            callback: Box::new(end_variable_key),
         },
         // VariableSeparator
         Transition {
             source: State::VariableSeparator,
             destination: State::VariableValue,
             condition: Regex::new(r"[^#\n \t]").unwrap(),
-            callback: Box::new(|mut p| start_variable_value(&mut p)),
+            callback: Box::new(start_variable_value),
         },
         Transition {
             source: State::VariableSeparator,
             destination: State::VariableValueLeadingWhitespace,
             condition: Regex::new(r"[\t ]").unwrap(),
-            callback: Box::new(|mut p| skip(&mut p)),
+            callback: Box::new(skip),
         },
         // VariableValueLeadingWhitespace
         Transition {
             source: State::VariableValueLeadingWhitespace,
             destination: State::VariableValue,
             condition: Regex::new(r"[^\t ]").unwrap(),
-            callback: Box::new(|mut p| start_variable_value(&mut p)),
+            callback: Box::new(start_variable_value),
         },
         // VariableValue
         Transition {
             source: State::VariableValue,
             destination: State::VariableValue,
             condition: Regex::new(r"[^#\n]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_variable_value(&mut p)),
+            callback: Box::new(add_char_to_variable_value),
         },
         Transition {
             source: State::VariableValue,
             destination: State::VariableValue,
             condition: Regex::new(r"[ \t]").unwrap(),
-            callback: Box::new(|mut p| add_char_to_variable_value(&mut p)),
+            callback: Box::new(add_char_to_variable_value),
         },
         Transition {
             source: State::VariableValue,
             destination: State::Comment,
             condition: Regex::new(r"#").unwrap(),
-            callback: Box::new(|mut p| end_variable_value(&mut p)),
+            callback: Box::new(end_variable_value),
         },
         Transition {
             source: State::VariableValue,
             destination: State::Newline,
             condition: Regex::new(r"\n").unwrap(),
-            callback: Box::new(|mut p| end_variable_value(&mut p)),
+            callback: Box::new(end_variable_value),
         },
     ];
     let test_file = "  
 #comment
 ratczar;80     #  test
+        score: 40
     dog: true
     dog2: true
 cattree
@@ -333,14 +332,13 @@ test
         parser.current_char = c;
         for t in &transitions {
             if t.source == parser.current_state && t.condition.is_match(c.to_string().as_str()) {
-                println!("Moving to {:?}, current c: {:?}", t.destination, c);
                 parser.current_state = t.destination;
                 (t.callback)(&mut parser);
                 break;
             }
         }
     }
-    parser.current_state = State::EOF;
+    parser.current_state = State::Eof;
     end_word(&mut parser);
 
     println!("{:?}", parser.parsed_words);
